97d8f5dc3b4d53bbc7a5f443b15be0381d1f4a81
Jakobi <jahardy2021@gmail.com>
2025-08-27 09:19:46 -0500

    directoory reorganized

--- node_modules/@clerk/backend/dist/api/resources/MachineSecretKey.d.ts
+++
@@ -1,7 +0,0 @@
-import type { MachineSecretKeyJSON } from './JSON';
-export declare class MachineSecretKey {
-    readonly secret: string;
-    constructor(secret: string);
-    static fromJSON(data: MachineSecretKeyJSON): MachineSecretKey;
-}
-//# sourceMappingURL=MachineSecretKey.d.ts.map
--- node_modules/@clerk/shared/dist/getEnvVariable.mjs
+++
@@ -1,8 +0,0 @@
-import {
-  getEnvVariable
-} from "./chunk-IBB3AM7F.mjs";
-import "./chunk-7ELT755Q.mjs";
-export {
-  getEnvVariable
-};
-//# sourceMappingURL=getEnvVariable.mjs.map
